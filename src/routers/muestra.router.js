c40eedce673381fd1013062218cc4b36f445b495
danielcordoba12 <dcordobaruiz@gmail.com>
2023-11-28 18:28:20 -0500

    backend

--- src/routers/muestra.router.js
+++ src/routers/muestra.router.js
@@ -1,13 +1,13 @@
 import {Router} from "express";
-import { check } from "express-validator";
+// import { check } from "express-validator";
 import { guardarMuestra,actualizarMuestra,buscarMuestra,listarMuestras,desactivarMuestra,activarMuestra } from "../controllers/muestras.controller.js";
-import {validarMuestra} from "../validation/muestras.validator.js";
+// import {validarMuestra} from "../validation/muestras.validator.js";
 import { validartoken } from "../controllers/autenticacion.controller.js";
 
 const muestraRoute = Router()
 
-muestraRoute.post("/registrar",validartoken,validarMuestra,guardarMuestra);
-muestraRoute.put("/actualizar/:id",validartoken,validarMuestra,actualizarMuestra);
+muestraRoute.post("/registrar",guardarMuestra);
+muestraRoute.put("/actualizar/:id",actualizarMuestra);
 muestraRoute.get("/buscar/:id",buscarMuestra);
 muestraRoute.get("/listar",listarMuestras);
 muestraRoute.patch("/desactivar/:id",validartoken,desactivarMuestra);
